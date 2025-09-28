0b8152ae5032d15b0670e9da8021bf6dea133965
quonaro <quonaro@mail.ru>
2025-09-29 01:34:06 +0700

    Update base path in Vite configuration for deployment to root directory

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/quonaro/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
