a00e3252bb306c66219602ce8e9666751d2cf64a
quonaro <quonaro@mail.ru>
2025-09-29 13:40:47 +0700

    Update Vite configuration to set base path to relative for local development

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/" : "/quonaro/",
+  base: "./",
   build: {
     outDir: "dist",
     assetsDir: "./", // Hi
