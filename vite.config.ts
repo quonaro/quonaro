d1997c6b141cf04c98e8c42341907093b0c1edd7
quonaro <quonaro@mail.ru>
2025-09-29 13:38:04 +0700

    Revert base path change in Vite configuration to use relative path for local development

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
   base: mode === "production" ? "/" : "/quonaro/",
-=======
-  base: "./",
->>>>>>> parent of 1f848c1 (Update Vite configuration to set base path for GitHub Pages deployment)
   build: {
     outDir: "dist",
     assetsDir: "./", // Hi
